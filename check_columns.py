cb4a162fbbff7e225677cbd3d73b4b54a93ca436
AYUSH SAHU <ayyushsahu2332@gmail.com>
2026-02-24 16:06:24 +0530

    check_columns.py

--- check_columns.py
+++ check_columns.py
@@ -1,19 +1,6 @@
-
-
-
-import os
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from flask import Flask, render_template, request
 
-app = Flask(__name__)
-
-# --- Load dataset safely ---
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # current folder
-data_path = os.path.join(BASE_DIR, "heart.csv")
-data = pd.read_csv(data_path)
-
-X = data.drop("condition", axis=1)
-y = data["condition"]
-model = RandomForestClassifier()
-model.fit(X, y)
+data = pd.read_csv("heart.csv")
+print("Columns in CSV:")
+for col in data.columns:
+    print(col)
