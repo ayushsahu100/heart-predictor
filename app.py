9bf327a565a4572ae0950032eaffd56ce840ceed
AYUSH SAHU <ayyushsahu2332@gmail.com>
2026-02-24 12:54:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
+import os
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
 app = Flask(__name__)
 
-# Load dataset and train model
-data = pd.read_csv("heart.csv")
+# --- Load dataset safely ---
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # current folder
+data_path = os.path.join(BASE_DIR, "heart.csv")
+data = pd.read_csv(data_path)
+
 X = data.drop("condition", axis=1)
 y = data["condition"]
 model = RandomForestClassifier()
